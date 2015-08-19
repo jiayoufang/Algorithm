d3e5a92b527a32f6357d4c5ea5487767fd3bf930
jiayoufang <1083432016@qq.com>
2015-08-19 12:57:57 +0800

    初始化项目

---
+++ JYSearchBarDemo/JYSearchBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYSearchBarDemo
+//
+//  Created by fangjiayou on 15/8/19.
+//  Copyright (c) 2015年 方. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYSearchBarDemo/JYSearchBarDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  JYSearchBarDemo
+//
+//  Created by fangjiayou on 15/8/19.
+//  Copyright (c) 2015年 方. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
